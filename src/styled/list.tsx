f2f0f3289be19aa3a3d0c4e9852ce0ff0873430c
dani <resina00@gmail.com>
2020-05-24 16:51:12 +0200

    commit to master, testing husky

---
+++ src/styled/list.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const STMatches = styled.ul`
+  background: red;
+`
+
+STMatches.displayName = 'STMatches'
+
+export {
+  STMatches
+}
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/app.ts'),
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader"
+          },
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      },
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        use: [
+          {
+            loader: "source-map-loader"
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.tsx', '.ts'],
+    alias: {
+     '@styled': path.resolve(__dirname, 'src/styled'),
+     '@widget': path.resolve(__dirname, 'src/components/widget'),
+     '@stlib': path.resolve(__dirname, 'src/st-lib'),
+     }
+  },
+  devtool: 'eval-source-map',
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
+}
